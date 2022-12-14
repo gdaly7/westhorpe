b8247e3d8f6052765e6e045ca1d817b49fbca430
gdaly7 <gdaly7@gmail.com>
2022-12-14 22:58:42 +0000

    new pathmaker link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -25,7 +25,8 @@ export default function Footer() {
                 <Link path='#!'>Privacy</Link>
               </Box>
               <Text as='p' sx={styles.copyright}>
-                Copyright by {new Date().getFullYear()} RedQ, Inc
+                Copyright by {new Date().getFullYear()} RedQ, Inc Web Design By <a href="https://pathmaker.dev">Pathmaker</a>
+
               </Text>
                 <Text as='p' sx={styles.copyright}>
                 Web Design By <a href="https://pathmaker.dev">Pathmaker</a>
